231b138785571d104c00682f9e00413bdbd70dfa
Ted Bennett <TeddyB@teds-macbook-pro.local>
2017-02-12 15:37:41 +0000

    Initial Commit

---
+++ MAME/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MAME
+//
+//  Created by Ted Bennett on 12/02/2017.
+//  Copyright © 2017 Ted Bennett. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
